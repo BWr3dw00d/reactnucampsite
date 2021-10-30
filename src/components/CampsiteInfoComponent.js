61377e6cf2b296d06fa7b69ccb15eb423fa29cdd
AJ Louis <burlywalter@gmail.com>
2021-10-30 17:04:03 -0400

    React Animation Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm , Errors} from "react-redux-form";
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Stagger, Fade } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -87,12 +88,18 @@ function RenderCampsite({campsite}) {
 
         return(
             <div className="col-md-5 m-1">
-               <Card>
-                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                    <Card>
+                        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             </div>
         );
     }
@@ -102,20 +109,23 @@ function  RenderComments({comments, postComment, campsiteId}) {
             return(
                  <div className="col-md-5 m-1">
                      <h4>Comments</h4>
-                     {comments.map(comment=>{
-                         return(
-                             <div>
-                                 <p>
-                                 {comment.text}
-                                 <br/>
-                                 --{comment.author} 
-                                 {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                 </p>
-                            </div> 
-                             
-                         );
-                     })}
-                     <CommentForm postComment={postComment} campsiteId={campsiteId} />
+                     <Stagger in>
+                    {
+                        comments.map(comment => {
+                            return (
+                                <Fade in key={comment.id}>
+                                    <div>
+                                        <p>
+                                            {comment.text}<br />
+                                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                        </p>
+                                    </div>
+                                </Fade>
+                            );
+                        })
+                    }
+                    </Stagger>
+                    <CommentForm postComment={postComment} campsiteId={campsiteId} />
                  </div>
 
             );
